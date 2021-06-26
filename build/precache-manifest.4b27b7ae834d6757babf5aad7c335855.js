af108f2524708f5141a215c3787f5850fd9c7d82
Syamantak Sarkar <49286743+s-decico@users.noreply.github.com>
2021-06-26 17:55:05 +0530

    Add files via upload

---
+++ build/precache-manifest.4b27b7ae834d6757babf5aad7c335855.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f866302b9d22c7fb5fcb1db964b1814c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a9911fbd73c719eba3f",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "eb9ee73de02d8fd6ea99",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "0a9911fbd73c719eba3f",
+    "url": "/static/js/2.cd3e8ec6.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.cd3e8ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb9ee73de02d8fd6ea99",
+    "url": "/static/js/main.71363915.chunk.js"
+  },
+  {
+    "revision": "5e2c0eb9a9791094e7a6",
+    "url": "/static/js/runtime-main.306f1d33.js"
+  }
+]);
